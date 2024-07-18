b935c00bf48864889d486186f35079797e89c453
unknown <Pranavnagawade>
2024-07-18 11:55:28 +0530

    Tester1 created the login page Test Case

---
+++ src/test/java/T1/Login.java
@@ -0,0 +1,11 @@
+package T1;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("login page is created by Tester1");
+
+	}
+
+}
