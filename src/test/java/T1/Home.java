8dfce69374f614aaeda39f5d0181b527cb9bcaa0
unknown <Pranavnagawade>
2024-07-18 12:05:47 +0530

    Taster2 add the the HomePage Test case

---
+++ src/test/java/T1/Home.java
@@ -0,0 +1,11 @@
+package T1;
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Taster2 creted the Home Page Test Case");
+
+	}
+
+}
